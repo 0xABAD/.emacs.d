75ca527131a106d9e1e49fdc9b94089f3a8189f7
Dan Abad <dan@metecs-0730.metecs.internal>
2023-01-26 16:03:11 -0600

    Update markdown-mode entry

--- init.el
+++ init.el
@@ -231,7 +231,7 @@ call 'cargo fmt'."
 (use-package markdown-mode
   :ensure t
   :mode ("README\\.md\\'" . gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
+  :init (setq markdown-command '("pandoc" "--from=markdown" "--to=html5")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
